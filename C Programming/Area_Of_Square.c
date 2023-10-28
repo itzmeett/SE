0895e217cac8552d4d412f59000c3d9fd1872f82
itzmeett <imeettpatel@gmail.com>
2023-10-28 11:50:56 +0530

    Added Classwork programs

--- C Programming/Area_Of_Square.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	float len;
-	printf("\nEnter the length of square = ");
-	scanf("%f", &len);
-	
-	printf("\nArea of square is = %.2f", len*len);
-}
--- C Programming/Intro.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	printf("\nHello there!! im Meet Patel");
-	printf("\nMy birthday is on 1st October");
-	printf("\nI am 21 years old");
-	printf("\nI am live at Shashtrinagar, Ahmedabad");
-}
