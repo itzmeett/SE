604b13fe8ac1427408999a167ca9ced341c64216
itzmeett <imeettpatel@gmail.com>
2023-11-01 10:33:32 +0530

    Update changesv in exit control loop

--- Assessment/C_Assessment_01.c
+++ Assessment/C_Assessment_01.c
@@ -1,46 +1,56 @@
 #include<stdio.h>
 main()
 {
-	int choice, n1, n2;
+	int choice; // choice for switch case
+	float n1, n2; //n1 & n2 declare for user input
+	
+	// Print the menu options for operations
 	printf("\n\n\n");
 	printf("-------------------Menu-------------------\n");
 	printf("\n 1. Addition");
 	printf("\n 2. Substraction");
 	printf("\n 3. Multiplication");
 	printf("\n 4. Division");
+	printf("\n 5. Exit");
 	
-	do{
+	// Using do while loop for exit menu
+	
+	while(choice !=5){
 	
 	printf("\n\nEnter your choice : ");
 	scanf("%d", &choice);
 	
 	printf("\nEnter First Number : ");
-	scanf("%d", &n1);
+	scanf("%f", &n1);
 	
 	printf("Enter Second Number : ");
-	scanf("%d", &n2);
+	scanf("%f", &n2);
 		
 	switch(choice)
 	{
 		case 1: { 
-			printf("\nAddition = %d", n1+n2);
+			printf("\nAddition = %.2f", n1+n2); 
 			printf("\n------------------------------------------");
 			break;
 		}
 		case 2:
 			{
-			printf("\nSubstraction = %d", n1-n2);
+			printf("\nSubstraction = %.2f", n1-n2);
 			printf("\n------------------------------------------");
 			break;
 			}
 		case 3:{
-			printf("\nMultiplication = %d", n1*n2);
+			printf("\nMultiplication = %.2f", n1*n2);
 			printf("\n------------------------------------------");
 			break;
 		}
 		case 4:{
-			printf("\nDivision = %d", n1/n2);
+			printf("\nDivision = %.2f", n1/n2);
 			printf("\n------------------------------------------");
+			break;
+		}
+		case 5:{
+			exit(1);
 			break;
 		}
 		default:{
@@ -49,6 +59,6 @@ main()
 			break;
 		}	
 	}
-	}while(2 > 1);
-
-}   
+	}
+}
+  
