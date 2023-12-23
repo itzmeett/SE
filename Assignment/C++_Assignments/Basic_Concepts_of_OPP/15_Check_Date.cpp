ca18a0fd6dc99c00ab4c97e221e713ad5be1b012
Meet Patel <imeettpatel@gmail.com>
2023-12-23 19:30:52 +0530

    Added assignment of Date check program

--- Assignment/C++_Assignments/Basic_Concepts_of_OPP/15_Check_Date.cpp
+++ Assignment/C++_Assignments/Basic_Concepts_of_OPP/15_Check_Date.cpp
@@ -16,22 +16,34 @@ class Date{
 	
 	public:
 		
-		void leapyear(int year)
+//		void getdata()
+//		{
+//			cout<<"Enter Day : ";
+//			cin>>day;
+//			
+//			cout<<"Enter Month : ";
+//			cin>>month;
+//			
+//			cout<<"Enter  Year: ";
+//			cin>>year;
+//		}
+		
+		int leapyear(int year) //it checks leap year or not 
 		{
 			if( (year%4 == 0) && (year%100 != 0) || (year%400 == 0) )
 			{
-				cout<<"\nThis "<<year<<" is leap year";
+				return year;
 			}
 		}
 		
-		void check_date(int dd, int mm, int yy)
+		int check_date(int dd, int mm, int yy) //it checks year and month are valid or not
 		{
 			
 			int day = dd;
 			int month = mm;
 			int year = yy;
 			
-			if(year > max_year || year < min_year)
+			if(year > max_year || year < min_year) // yeaar is true if year is between 1800 to 9999
 			{
 				return false;
 			}
@@ -44,7 +56,7 @@ class Date{
 				return false;
 			}
 			
-			if(month == 2){
+			if(month == 2){ // month is true if it is between 1 to 12
 				if(leapyear(year))
 				{
 					return	(day <= 29);
@@ -54,11 +66,12 @@ class Date{
 				}
 			}
 			
-			if(month == 4 || month == 6 || month == 9 || month == 11 )
+			if(month == 4 || month == 6 || month == 9 || month == 11)
 			{
 				return (day <= 30);
 				return true;
-			}		
+			}	
+				
 		}
 };
 
@@ -66,8 +79,12 @@ main()
 {
 	int dd, mm, yy;
 	
-	Date check1;
+	Date check1; //check1 object can access "Date" class methods.
 	
+//	check1.getdata();
 	check1.leapyear(2002);
-	check1.leapyear(29, 02, 2002);
+	check1.check_date(31, 10, 2002) ? cout << "\nDate is valid" : cout<<"\nDate is not valid"; 
+	check1.check_date(32, 11, 22023) ? cout << "\nDate is valid" : cout<<"\nDate is not valid";
+	check1.check_date(30, 02, 2023) ? cout << "\nDate is valid" : cout<<"\nDate is not valid";
+	
 }
